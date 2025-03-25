51eedb9a975c6e8bf3d5a55861fcceaadde8d335
Veljko Belic <veljkobelic2002@gmail.com>
2025-03-25 16:22:47 +0100

    Adding header component to application

---
+++ web-app/src/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    <div className="header">
+        <div className="headingBox">
+            <h1>Notes</h1>
+        </div>
+    </div>
+}
+
+export default Header;
