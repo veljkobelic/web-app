d29ca75aeb1434572e43193ef9618c18eabe1da9
Veljko Belic <veljkobelic2002@gmail.com>
2025-03-25 16:14:33 +0100

    Deleting unnecessary files, creating new main file

--- web-app/src/main.jsx
+++ web-app/src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDom from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render();
