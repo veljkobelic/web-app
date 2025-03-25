85c0e5d4e9c92ae9d0524d2ea9b24c3a47fe3744
Veljko Belic <veljkobelic2002@gmail.com>
2025-03-25 16:36:18 +0100

    Adding stylesheets folder and css file for the header

--- web-app/src/main.jsx
+++ web-app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import Header from "./header";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render();
