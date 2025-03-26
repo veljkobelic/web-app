e7a89626dee80e3466a45a8fb4f464f13b576009
Veljko Belic <veljkobelic2002@gmail.com>
2025-03-26 06:58:49 +0100

    Adding missing return statement to function

--- web-app/src/header.jsx
+++ web-app/src/header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Header(){
-    <div className="header">
+    return (<div className="header">
         <div className="headingBox">
             <h1>Notes</h1>
         </div>
-    </div>
+    </div>);
 }
 
 export default Header;
