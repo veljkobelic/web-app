25f777c8ce2f08bb4b8a640e392fb171b13c53f3
Veljko Belic <veljkobelic2002@gmail.com>
2025-03-27 21:24:05 +0100

    Import footer and Note Card

--- web-app/src/main.jsx
+++ web-app/src/main.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
-    <Header/>
+    <>
+        <Header/>
+        <Note/>
+        <Footer/>
+    </>
 );
