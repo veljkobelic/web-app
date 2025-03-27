bdc6c4721197964c2a3d9b61eb83c1e7566aebdb
Veljko Belic <veljkobelic2002@gmail.com>
2025-03-27 21:23:39 +0100

    Adding more elements to header

--- web-app/src/header.jsx
+++ web-app/src/header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 function Header(){
-    return (<div className="header">
+    return (
+    <div className="header">
         <div className="headingBox">
-            <h1>Notes</h1>
+            <h1 className="heading">Notes</h1>
+            <div className="navbar">
+                <h3>Dashboard</h3>
+                <h3>Account Details</h3>
+            </div>
         </div>
     </div>);
 }
